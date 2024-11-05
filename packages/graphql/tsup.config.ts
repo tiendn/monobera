3da987b13936ad87a77d175f9f754ad21627247d
bearpong <178402093+bearpong@users.noreply.github.com>
2024-11-05 19:15:58 +0100

    ci: fix vercel out of memory

--- packages/graphql/tsup.config.ts
+++ packages/graphql/tsup.config.ts
@@ -16,7 +16,7 @@ const config = {
   minify: process.env.NODE_ENV !== "development", // Whether to minify the output
   sourcemap: process.env.NODE_ENV !== "development", // Whether to generate sourcemaps
   // splitting: true, // Whether to split the bundle into chunks
-  dts: true, // Whether to generate TypeScript declaration files
+  dts: process.env.VERCEL !== "1", // Whether to generate TypeScript declaration files
   // target: "node18", // Specify your target environment
 
   async onSuccess() {
