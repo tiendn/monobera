76562c267fc6a1295a08508f53d630933777c622
Paul McInnis <paulmcinnis99@gmail.com>
2024-11-11 18:57:58 -0500

    feat(bex): update swap-fee component styling

--- packages/shared-ui/src/swap-fee.tsx
+++ packages/shared-ui/src/swap-fee.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from "react";
 import { cn } from "@bera/ui";
+import { Input } from "@bera/ui/input";
 
 interface SwapFeeInputProps {
   initialFee?: number;
@@ -38,31 +39,29 @@ export function SwapFeeInput({
 
   return (
     <section className="flex w-full flex-col gap-6">
-      <div className="flex flex-col gap-4">
-        <div className="relative">
-          <input
-            type="number"
-            value={fee}
-            onChange={(e) => handleFeeChange(e.target.value)}
-            placeholder="Enter swap fee"
-            className={cn(
-              "w-full rounded-md border p-2 pr-10 text-2xl",
-              isInvalid
-                ? "border-destructive-foreground text-destructive-foreground"
-                : "border-border",
-            )}
-            aria-label="Swap Fee Input"
-          />
-          <span
-            className={cn(
-              "absolute right-4 top-1/2 -translate-y-1/2 transform text-lg text-gray-500",
-              isInvalid && "text-destructive-foreground",
-            )}
-          >
-            %
-          </span>
-        </div>
-
+      <div className="relative flex flex-row gap-6">
+        <Input
+          type="number"
+          variant="black"
+          value={fee}
+          onChange={(e) => handleFeeChange(e.target.value)}
+          placeholder="Enter swap fee"
+          className={cn(
+            "w-full rounded-md border bg-transparent p-2 pr-10",
+            isInvalid
+              ? "border-destructive-foreground text-destructive-foreground"
+              : "border-border",
+          )}
+          aria-label="Swap Fee Input"
+        />
+        <span
+          className={cn(
+            "absolute left-1/2 top-1/2 -translate-y-1/2 transform text-gray-500",
+            isInvalid && "text-destructive-foreground",
+          )}
+        >
+          %
+        </span>
         <div className="flex gap-2">
           {predefinedFees.map((preset) => (
             <button
@@ -79,14 +78,14 @@ export function SwapFeeInput({
             </button>
           ))}
         </div>
-
-        {isInvalid && (
-          <div className="mt-2 rounded-md border border-destructive-foreground p-2 text-sm text-destructive-foreground">
-            <i className="mr-2">⚠️</i>
-            Invalid fee. Ensure the entered fee is between 0.00001% and 10%.
-          </div>
-        )}
       </div>
+
+      {isInvalid && (
+        <div className="mt-2 rounded-md border border-destructive-foreground p-2 text-sm text-destructive-foreground">
+          <i className="mr-2">⚠️</i>
+          Invalid fee. Ensure the entered fee is between 0.00001% and 10%.
+        </div>
+      )}
     </section>
   );
 }
