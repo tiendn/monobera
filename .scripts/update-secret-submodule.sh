ec99e0dbe239d250320d4d4a54e772753fc8de87
chuck-bear <178402093+bearpong@users.noreply.github.com>
2024-11-18 20:07:25 +0100

    fix: b-sdk submodule was missing

--- .scripts/update-secret-submodule.sh
+++ .scripts/update-secret-submodule.sh
@@ -11,13 +11,13 @@ SUBMODULE_NAME="secrets"
 
 AUTH_URL="https://$SECRET_SUBMODULE_PAT@github.com/berachain/internal-dapps-env.git"
 
-rm -rf secrets
 
 # Update the submodule URL using git commands
-git submodule set-url secrets "$AUTH_URL"
+git submodule set-url "$SUBMODULE_NAME" "$AUTH_URL"
 
 # Sync and update the submodule in the Git repository
-git submodule sync "$SUBMODULE_NAME"
-git submodule update --init --recursive
+# This looks redundant, but it's a workaround for missing b-sdk submodule
+git submodule deinit --quiet --all -f
+git submodule update --quiet --init --recursive
 
 # echo "The URL of the submodule '$SUBMODULE_NAME' has been updated to '$AUTH_URL'."
