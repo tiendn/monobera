7d8f96e44d801194a8d65ed0c7fb9b3134499d7a
brown <brown@berachain.com>
2024-11-18 18:19:30 -0500

    feat(hub): create and check for already created gauges based on staking tokens

--- apps/hub/src/app/vaults/components/gauge-tables.tsx
+++ apps/hub/src/app/vaults/components/gauge-tables.tsx
@@ -25,7 +25,7 @@ export default function GaugeTables() {
   }, [typingTimer]);
 
   return (
-    <Tabs defaultValue="all-gauges" className="flex flex-col gap-4">
+    <Tabs defaultValue="all-gauges" className="flex flex-col gap-4 mt-4">
       <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
         <TabsList className="w-full md:w-fit" variant="ghost">
           <TabsTrigger value="all-gauges" className="w-full md:w-fit">
