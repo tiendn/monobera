6e190ad16bed98499d8e90efff2dc4c54ddc8848
chuck-bear <178402093+bearpong@users.noreply.github.com>
2024-11-21 14:04:20 +0100

    chore: adds DONT_INDEX in env to disallow SE robots Refs BFE-305

--- apps/hub/src/app/robots.ts
+++ apps/hub/src/app/robots.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from "next";
 import { isIPFS } from "@bera/config";
 
 export default function robots(): MetadataRoute.Robots {
-  if (isIPFS) {
+  if (isIPFS || process.env.DONT_INDEX === "1") {
     return {
       rules: {
         userAgent: "*",
