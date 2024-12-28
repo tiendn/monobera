b61ef00fa4a688175c33be1fa2ab0ac0fc92522e
tiendn <15717476+tiendn@users.noreply.github.com>
2024-12-29 00:39:23 +0700

    test: update jest coverage config

--- packages/berajs/jest.config.js
+++ packages/berajs/jest.config.js
@@ -5,7 +5,26 @@ export default {
     "**/?(*.)+(spec|test).+(ts|tsx|js)",
   ],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.(ts|tsx|js)$": [
+      "ts-jest",
+      {
+        tsconfig: "<rootDir>/tsconfig.json",
+        isolatedModules: true,
+        useESM: true,
+      },
+    ],
   },
   testEnvironment: "node",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node", "mjs"],
+  collectCoverage: true,
+  coverageDirectory: "<rootDir>/coverage",
+  coverageReporters: ["text", "lcov"],
+  coveragePathIgnorePatterns: ["/node_modules/", "/dist/"],
+  clearMocks: true,
+  resetMocks: true,
+  moduleNameMapper: {
+    "^~/(.*)$": "<rootDir>/src/$1",
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  transformIgnorePatterns: ["node_modules/"],
 };
